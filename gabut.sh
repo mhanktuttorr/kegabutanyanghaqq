bcfd0eb9ad39fdc1b73d162b07994f5f4f036268
mhanktuttorr <54797454+mhanktuttorr@users.noreply.github.com>
2019-09-03 05:06:45 +0700

    Add files via upload

---
+++ gabut.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+clear
+r='\033[31;1m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;1m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+
+# [Coded By] Rizki18, Youtube : Mhank Tuttorr
+
+echo $p""
+figlet -f standard GABUTERs
+echo
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo $p"["$w"+"$p"]"$w" Dibuat khusus untuk Hacker pro"
+echo $p"["$w"+"$p"]"$w" Dilarang menggunakan tools ini"
+echo $p"["$w"+"$p"]"$w" Kecuali lagi "$p"gabut ..."
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo
+echo $p"["$w"1"$p"]"$w" Lirik Lagu lihat kebunku"$p" v.1"
+
+echo $p"║"
+read -p"╚═[Pilih]═➢" pil
+
+if [ $pil = 1 ]
+then
+clear
+echo $p""
+figlet -f slant "WAIT"
+sleep 1
+echo $p"["$w"+"$p"]" $w"Sedang Mencari Ruang karoke"
+sleep 1
+echo $p"["$w"+"$p"]" $w"Menuju tak terbatas dan melampaui nya"
+sleep 1
+echo $p"["$w"+"$p"]" $w"..."
+sleep 1
+clear
+sleep 1
+echo $p"Termux Karouke" 
+echo $w"Lirik lagu Lihat kebunku versi Mod"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Lihat Pacarku"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Dia main gila"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Punya kekasih"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Kok tidak setia"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Setiap hari"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Ku doakan dia"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Semoga mati"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Keseruduk beca !"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Beca, beca !"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" coba bawa saya"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" saya mau tamasya"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" berkliling keliling kota"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Hendak melihat lihat"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Kegiatan si doi"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Lihat beca ku lari !"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Kereta tak berhenti"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Beca, beca !"
+echo
+sleep 2
+echo $p"["$w""$p"]" $w" Tabrak doi lagi ! :("
+echo
+sleep 2
+echo $p"Terimakasih"$w" Atas kegabutan kalian"
+echo $w"            Yang sudah menggunakan tools unfaedah ini"
+echo $p"Dasar"
+sleep 1
+echo $w"Gabut ..."
+echo $p"Dont Forget To Subscribe Channel Mhank Tuttorr"
+echo $w"WAJIB"
+sh l.sh
+fi
